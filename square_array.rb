97d1c2b1ada10074966cc4692f9fed5090eb0e08
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-12 11:07:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,17 @@
 def square_array(array)
- 
+ array.each_with_index do |number,index|
+   square_number  = number**2
+   array[index] = square_number
+ end
+ array
 end
 
+  #array.each_with_index do |number,index|
+  #  square_number  = number**2
+   # array[index] = square_number
+   #puts "index of #{number} is #{index}"
+  #end
+ # puts "#{array}"
+
+
+#square_array([2,4,5])
